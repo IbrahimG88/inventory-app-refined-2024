00aedcdd787b9add214f7676e727193f4190ee0a
IbrahimG88 <goharyibrahim@gmail.com>
2024-03-13 02:04:41 +0200

    added cors

--- pages/api/testsCRUD/postTestsList.js
+++ pages/api/testsCRUD/postTestsList.js
@@ -1,6 +1,17 @@
-import { connectToDatabase } from "../../lib/db";
+import { connectToDatabase } from "../../../lib/db";
+import initMiddleware from "../../../lib/init-middleware";
+
+// Initialize the cors middleware
+const cors = initMiddleware(
+  // You can read more about the available options here: https://github.com/expressjs/cors#configuration-options
+  Cors({
+    // Only allow requests with GET, POST and OPTIONS
+    methods: ["GET", "POST", "OPTIONS"],
+  })
+);
 
 export default async function handler(req, res) {
+  await cors(req, res);
   try {
     if (req.method !== "POST") {
       throw { status: 400, message: "Invalid request method" };
